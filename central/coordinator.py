97b533357862354e44bad4499dd4690608b609a9
Thanva Phupingbut <rufiodinoto244@gmail.com>
2025-08-19 17:48:35 +0700

    Update coordinator.py

--- central/coordinator.py
+++ central/coordinator.py
@@ -1,1 +1,10 @@
+from collections import defaultdict
 
+class Coordinator:
+    def __init__(self):
+        self.tasks = defaultdict(list)
+
+    def store_agent_task(self, agent_name, task):
+        self.tasks[agent_name].append(task)
+
+coordinator = Coordinator()
