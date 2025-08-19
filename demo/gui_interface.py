e9ddef17c8faa19a8dbf78320e65f5712f7ec52f
Thanva Phupingbut <rufiodinoto244@gmail.com>
2025-08-19 18:16:56 +0700

    Update gui_interface.py

--- demo/gui_interface.py
+++ demo/gui_interface.py
@@ -1,1 +1,25 @@
+import cv2
+import pyttsx3
+import threading
 
+class GUI:
+    def __init__(self):
+        self.tts_engine = pyttsx3.init()
+        self.running = True
+
+    def display_video(self, frame):
+        cv2.imshow("Scanzaclip Live Demo", frame)
+        cv2.waitKey(1)
+
+    def speak(self, text):
+        self.tts_engine.say(text)
+        self.tts_engine.runAndWait()
+
+    def run_video_loop(self, video_source=0):
+        cap = cv2.VideoCapture(video_source)  # 0 = webcam / scanzaclip camera feed
+        while self.running:
+            ret, frame = cap.read()
+            if ret:
+                self.display_video(frame)
+        cap.release()
+        cv2.destroyAllWindows()
