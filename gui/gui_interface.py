3d78cc7f42212b70896c3ad77f8d59af15955e8d
Thanva Phupingbut <rufiodinoto244@gmail.com>
2025-08-19 17:48:01 +0700

    Update gui_interface.py

--- gui/gui_interface.py
+++ gui/gui_interface.py
@@ -1,1 +1,11 @@
+class GUI:
+    def __init__(self):
+        self.memory_display = self
 
+    def update_gui(self, action, emotion, memory_text=None):
+        print(f"[GUI] Action: {action}, Emotion: {emotion}")
+
+    def run(self):
+        print("[GUI] Running GUI loop...")
+        while True:
+            pass  # GUI loop
