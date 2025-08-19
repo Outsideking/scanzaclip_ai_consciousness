fb3d2bf6c57a61555952bfaf794fb9db113996cd
Thanva Phupingbut <rufiodinoto244@gmail.com>
2025-08-19 19:01:25 +0700

    Update coordinator.py

--- central/coordinator.py
+++ central/coordinator.py
@@ -8,3 +8,18 @@ class Coordinator:
         self.tasks[agent_name].append(task)
 
 coordinator = Coordinator()
+class Coordinator:
+    def __init__(self, ws_url):
+        self.ws_url = ws_url
+        self.agent_tasks = {}  # เก็บ task ของทุก agent
+
+    def send_task(self, task):
+        agent = task.get("agent","unknown")
+        if agent not in self.agent_tasks:
+            self.agent_tasks[agent] = []
+        self.agent_tasks[agent].append(task)
+        # ส่งไป WebSocket server ปกติ
+        try:
+            self.ws.send(json.dumps(task))
+        except:
+            pass
