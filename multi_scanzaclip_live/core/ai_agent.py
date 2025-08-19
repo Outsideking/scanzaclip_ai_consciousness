64a7e23468224e32aedfd95294148c9d5a89d8c2
Thanva Phupingbut <rufiodinoto244@gmail.com>
2025-08-19 18:38:07 +0700

    Update ai_agent.py

--- multi_scanzaclip_live/core/ai_agent.py
+++ multi_scanzaclip_live/core/ai_agent.py
@@ -1,1 +1,11 @@
-
+def decide_action(emotion):
+    if emotion == "sad":
+        action = "cheer_up_user"
+        response_text = "Don't worry, everything will be fine!"
+    elif emotion == "happy":
+        action = "observe"
+        response_text = "You seem happy, observing environment."
+    else:
+        action = "observe"
+        response_text = "Observing environment."
+    return action, response_text
