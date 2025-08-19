33a0e8a47b3164d4a8653a1e5bc78a6d91dc1410
Thanva Phupingbut <rufiodinoto244@gmail.com>
2025-08-19 17:45:15 +0700

    Update emotion_decision_tree.py

--- core/emotion_decision_tree.py
+++ core/emotion_decision_tree.py
@@ -1,1 +1,19 @@
+class EmotionDecisionTree:
+    def __init__(self):
+        self.tree = {}
 
+    def add_rule(self, current_emotion, user_state, action, weight=1.0):
+        if current_emotion not in self.tree:
+            self.tree[current_emotion] = []
+        self.tree[current_emotion].append({
+            "user_state": user_state,
+            "action": action,
+            "weight": weight
+        })
+
+    def decide_action(self, user_id):
+        # ตัวอย่าง simple random pick
+        from random import choice
+        actions = self.tree.get("neutral", [{"action":"observe","weight":1.0}])
+        action = choice(actions)
+        return action["action"], "neutral"
